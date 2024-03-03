24fa93e4b1c7287042021ecc6519ec25fefb098e
ds3783 <ds3783@gmail.com>
2024-03-03 09:11:34 +0800

    add healthcheck

--- server/src/bin/app.mjs
+++ server/src/bin/app.mjs
@@ -135,7 +135,7 @@ export default async function () {
             CronJobs(false);
         }
 
-        app.use('*/healthcheck.html', express.static(path.join(__dirname, '../healthcheck.html'), {
+        app.use('*/healthcheck.html', express.static(path.resolve(path.join('healthcheck.html')), {
             fallthrough: false
         }));
         
